1e4b33b942ec70d49e9380ac8206e7ad37bd5fb9
Joost Kolb <joostkolb@hotmail.com>
2021-04-02 18:32:17 +0200

    Add files via upload

---
+++ Opdracht 2 - Javascript in Node/script.js
@@ -0,0 +1,27 @@
+console.log("Hallo winc Academy");
+
+// Deel 2
+// Opdracht 1
+// Dit is een grote som
+let som1 = 1230941 * 1823794;
+console.log(som1);
+
+// Opdracht 2
+let som2 = 637263 / 54;
+console.log(som2);
+
+// Opdracht 3
+// Opdracht 4
+// Nee dit zie je niet want dit is uitgecommentarieerd.
+
+//Opdracht 5
+/*
+let som1 = 1230941 * 1823794;
+console.log(som1);
+
+let som2 = 637263 / 54;
+console.log(som2);
+*/
+
+// Opdracht 6:
+// Nee dit zie je niet want dit is uitgecommentarieerd.
